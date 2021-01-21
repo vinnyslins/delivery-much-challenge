5f87bfbd1eac7d2ce5ea347023b5e8baceec0171
Vinnys Lins <vinnysflins@gmail.com>
2021-01-21 01:51:11 -0300

    fix: custom joi

--- src/utils/CustomJoi.ts
+++ src/utils/CustomJoi.ts
@@ -2,7 +2,7 @@ import { Joi } from 'celebrate'
 
 const CustomJoi = Joi.extend(joi => ({
   type: 'stringArray',
-  base: joi.array(),
+  base: joi.array().required(),
   coerce: value => ({
     value: value
       ? value.split(',').map((item: string) => item.trim())
