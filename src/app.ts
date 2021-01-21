bfc8765d9ade2e9483c9315eb534c2a8d7345586
Vinnys Lins <vinnysflins@gmail.com>
2021-01-21 00:22:09 -0300

    docs: add swagger

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,19 @@
 import 'dotenv/config'
+import path from 'path'
 import express from 'express'
 import 'express-async-errors'
+import yaml from 'yamljs'
+import swaggerUi from 'swagger-ui-express'
 
 import RecipeRoutes from './routes/Recipe'
 import Exception from './middlewares/Exception'
+
+const swaggetDocument = yaml.load(path.join(__dirname, '../swagger.yaml'))
 
 const app = express()
 
 app.use('/api', RecipeRoutes)
 app.use(Exception)
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggetDocument))
 
 export default app
